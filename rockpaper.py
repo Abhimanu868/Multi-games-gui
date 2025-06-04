cb2c4d0c1e7b512317707e9ac62619f6606613c0
Abhimanyu <abhimanu86850@gmail.com>
2025-06-04 16:47:35 +0530

    One Application for multiple games

--- rockpaper.py
+++ rockpaper.py
@@ -1,5 +1,6 @@
 import random
 import tkinter as tk
+from tkinter import messagebox
 import pygame
 import os
 import sys
@@ -11,43 +12,35 @@ def resource_path(relative_path):
         base_path = os.path.abspath(".")  # Normal Python execution
 
     return os.path.join(base_path, relative_path)
-root = tk.Tk()
-#creating window
-root.geometry("400x500")
-root.title("Welcome to my game")
-root.config(bg="lightblue")
-#defining globally
-your_score = 0
-computer_score = 0
-choices = ['rock','paper','scissors']
-dark_mode = True
+#global scopes
 pl_music = True
+music_length = 0.5
 enable_music = True
-music_level = 0.5
-#creating labels
-title_label = tk.Label(root,text="Rock,Paper,Scissors",font=("Arial",16,"bold"),bg="#e6f7ff",fg="red")
-title_label.pack(pady=20)
-score_label = tk.Label(root,text="Your Score: 0 / Computer Score: 0",font=("Arial",16,'bold'),bg="#e6f7ff")
-score_label.pack(pady=10)
-result_label = tk.Label(root,text="",font=("Arial",16,'bold'),bg="#e6f7ff")
-result_label.pack(pady=10)
-user_choice_label = tk.Label(root,text="Your choice: ",font=("Arial",16,"bold"),bg="#e6f7ff")
-user_choice_label.pack(pady=5)
-comp_choice_label = tk.Label(root,text="Computer choice: ",font=("Arial",16,'bold'),bg="#e6f7ff")
-comp_choice_label.pack(pady=5)
-rounds_play_label = tk.Label(root,text="Total Rounds: 0/4",font=("Arial",16,'bold'),bg="#e6f7ff")
-rounds_play_label.pack(pady=5)
-#main functionality
-pygame.mixer.init() # intializing the pygame mixer
-#adding on click sound feature
+enable_mode = False
+def change_mode():
+    global enable_mode
+    if enable_mode:
+        return {'bg':'#2e2e2e','fg':'white'}
+    else:
+        return {'bg':'white','fg':'#2e2e2e'}
+def apply_to_any_window(win):
+    change_colors = change_mode()
+    win.configure(bg=change_colors['bg'])
+    for widget in win.winfo_children():
+        try:
+            widget.configure(bg=change_colors['bg'],fg=change_colors['fg'])
+        except:
+            pass
+#music
+pygame.mixer.init()
 def on_click():
+    global enable_music
     if enable_music:
         try:
             click_sound = pygame.mixer.Sound(resource_path("computer-mouse-click-351398.mp3"))
             click_sound.play()
         except Exception as e1:
             print("Error on click music")
-#adding play music feature on clicking
 def play_music():
     global pl_music
     try:
@@ -61,109 +54,208 @@ def play_music():
             pl_music=True
     except Exception as e:
         print("Music error:",e)
-rounds=4
-current_round=0
-#main game 
-def play_game(user_choice):
-    global your_score,computer_score,choices,rounds,current_round
-    if(current_round>=rounds):
-        return
-    computer = random.choice(choices)
-    user_choice_label.config(text=f"Your choice: {user_choice}")
-    comp_choice_label.config(text=f"Computer choice: {computer}")
-    if user_choice==computer:
-        result = "Its a Tie"
-    elif((user_choice=='rock'and computer=='scissors')or(user_choice=='paper'and computer=='rock')or(user_choice=='scissors'and computer=='paper')):
-        your_score+=1
-        result = 'You won this round'
-    else:
-        computer_score+=1
-        result='Computer won this round'
-    result_label.config(text=result)
-    score_label.config(text=f"Your Score: {your_score} / Computer Score: {computer_score}")
-    current_round+=1
-    rounds_play_label.config(text=f"Total Rounds: {current_round}/{rounds}")
-    if(current_round==rounds):
-        end_game()
-def end_game():
-    global your_score,computer_score
-    if(your_score==computer_score):
-        result = 'Its a Tie!!'
-    elif(your_score<computer_score):
-        result = 'Win for Computer!!'
-    else:
-        result = 'Congratulations You won the game!!'
-    result_label.config(text=result)
-#for reseting
-def reset_game():
-    global your_score,computer_score,current_round
-    your_score=0
-    computer_score=0
-    current_round=0
-    score_label.config(text="Your Score: 0 / Computer Score: 0")
-    result_label.config(text="")
-    user_choice_label.config(text="Your choice: ")
-    comp_choice_label.config(text='Computer choice: ')
-    rounds_play_label.config(text='Total Rounds: 0/4')
-#adding features on exit button
-def on_exit():
-    on_click()
-    destroy = root.destroy
-    root.after(300,destroy)
-#settings menu
+
+#rock-paper-scissor
+play_list = ['rock','paper','scissor']
+class rockpaperscissor:
+    def __init__(self):
+        self.player_score = 0
+        self.computer_score = 0
+        self.rounds = 6
+        self.current_round=0
+        self.window = tk.Tk()
+        self.window.geometry('500x500')
+        self.window.title('Welcome to my game')
+        self.top_label = tk.Label(self.window,text='Rock,Paper,Scissors',font=('Arial',16,'bold'))
+        self.top_label.pack(pady=5)
+        self.winner_label = tk.Label(self.window,text='')
+        self.winner_label.pack(pady=5)
+        self.your_choice = tk.Label(self.window,text='Your choice: ',font=('Arial',16,'bold'))
+        self.your_choice.pack(pady=5)
+        self.computer_choice = tk.Label(self.window,text='Computer choice: ',font=('Arial',16,'bold'))
+        self.computer_choice.pack(pady=5)
+        self.score_label = tk.Label(self.window,text=f'Your score: 0/Computer score: 0',font=('Arial',16,'bold'))
+        self.score_label.pack(pady=5)
+        self.rounds_label = tk.Label(self.window,text=f'Rounds: 0/6',font=('Arial',16,'bold'))
+        self.rounds_label.pack(pady=5)
+        self.button_frame = tk.Frame(self.window,bg="#e6f7ff")
+        self.button_frame.pack(pady=20)
+        self.rock_button = tk.Button(self.button_frame, text="Rock",height=2, width=15, command=lambda: [on_click(),self.play_game("rock")])
+        self.rock_button.grid(row=0,column=0,pady=5,padx=5)
+
+        self.paper_button = tk.Button(self.button_frame, text="Paper",height=2,  width=15, command=lambda: [on_click(),self.play_game("paper")])
+        self.paper_button.grid(row=0,column=1,pady=5,padx=5)
+
+        self.scissor_button = tk.Button(self.button_frame, text="Scissor",height=2,  width=15, command=lambda: [on_click(),self.play_game("scissor")])
+        self.scissor_button.grid(row=0,column=2,pady=5,padx=5)
+        self.reset_button = tk.Button(self.button_frame, text="Reset",height=2,  width=15, command=lambda: [on_click(),self.reset_game()])
+        self.reset_button.grid(row=1,column=0,pady=5,padx=5)
+        self.exit_button = tk.Button(self.button_frame,text='Back',height=2, width=15,command=self.on_back)
+        self.exit_button.grid(row=1,column=1,pady=5,padx=5)
+        apply_to_any_window(self.window)
+    def play_game(self,user_input):
+        if self.current_round>=self.rounds:
+            return
+        self.comp_choice = random.choice(play_list)
+        self.your_choice.config(text=f'Your choice: {user_input}')
+        self.computer_choice.config(text=f'Computer choice: {self.comp_choice}')
+        if user_input==self.comp_choice:
+            self.winner_label.config(text='Its a tie')
+        elif(user_input=='rock' and self.comp_choice=='scissor')or(user_input=='paper' and self.comp_choice=='rock')or(user_input=='scissor' and self.comp_choice=='paper'):
+            self.winner_label.config(text='You won this round')
+            self.player_score+=1
+        else:
+            self.winner_label.config(text='computer won this round') 
+            self.computer_score+=1
+        self.score_label.config(text=f'Your score: {self.player_score}/Computer score: {self.computer_score}')
+        self.current_round+=1
+        self.rounds_label.config(text=f'Rounds: {self.current_round}/{self.rounds}')
+        
+        if self.current_round == self.rounds:
+            self.decide_winner()
+    def decide_winner(self):
+        if self.player_score>self.computer_score:
+            self.winner_label.config(text='You won the game')
+        elif(self.player_score==self.computer_score):
+            self.winner_label.config(text='Its tie better luck next time')
+        else:
+            self.winner_label.config(text='Computer won the game')
+    def reset_game(self):
+        self.player_score = 0
+        self.computer_score=0
+        self.current_round=0
+        self.winner_label.config(text='')
+        self.score_label.config(text='Your score: 0/Computer score: 0')
+        self.rounds_label.config(text='Rounds: 0/6')
+    def on_back(self):
+        self.click= on_click()
+        destroy = self.window.destroy
+        self.window.after(300,destroy)
+'''Hangman game'''
+word_list = ['apple','grape', 'lemon', 'mango', 'peach',
+    'berry', 'plum', 'melon', 'chili', 'onion',
+    'carrot', 'bread', 'cheese', 'toast', 'cream',
+    'chair', 'table', 'couch', 'glass', 'plate',
+    'knife', 'spoon', 'cup', 'fork', 'plant',
+    'stone', 'river', 'ocean', 'beach', 'cloud',
+    'rain', 'storm', 'light', 'shade', 'flame',
+    'brush', 'paint', 'brick', 'steel', 'wheel',
+    'train', 'plane', 'truck', 'sheep', 'horse',
+    'tiger', 'zebra', 'eagle', 'panda', 'mouse']
+class Hangman:
+    def __init__(self):
+        self.roo = tk.Tk()
+        self.roo.geometry('500x500')
+        global word_list
+        self.word = random.choice(word_list)
+        self.guessed_letters = ''
+        self.tries = 6
+        self.word_label = tk.Label(self.roo,text='_ '*len(self.word),font=('Arial',16,'bold'))
+        self.word_label.pack(pady=5)
+        self.enter = tk.Entry(self.roo,font=('Arial',16,'bold'))
+        self.enter.pack(pady=5)
+        self.enter_button = tk.Button(self.roo,text='Enter',width=15,font=('Arial',16,'bold'),command=lambda:[on_click(),self.play_game()])
+        self.enter_button.pack(pady=5)
+        self.feedback_label = tk.Label(self.roo,text='')
+        self.feedback_label.pack(pady=5)
+        self.tries_label = tk.Label(self.roo,text=f'Your tries left: {self.tries}/6')
+        self.tries_label.pack(pady=5)
+        self.reset_button = tk.Button(self.roo,text='Reset',width=15,command=lambda:[on_click(),self.reset_game()])
+        self.reset_button.pack(pady=5)
+        self.back_button = tk.Button(self.roo,text='Back',width=15,command=self.on_back)
+        self.back_button.pack(pady=5)
+        apply_to_any_window(self.roo)
+        self.display_update()
+    def display_update(self):
+        guessed_word = ''
+        for letter in self.word:
+            if letter in self.guessed_letters:
+                guessed_word+=letter+" "
+            else:
+                guessed_word+='_ '  
+        self.word_label.config(text=guessed_word.strip())  
+    def play_game(self):
+        guess = self.enter.get().lower()
+        self.enter.delete(0,tk.END)
+        if (not guess.isalpha()) or len(guess)!=1:
+            self.feedback_label.config(text='Invalid choice!')
+            return
+        if guess in self.guessed_letters:
+            self.feedback_label.config(text='you already guessed')
+            return
+        self.guessed_letters+=guess
+        if guess in self.word:
+            self.feedback_label.config(text='correct')
+        else:
+            self.tries-=1
+            self.tries_label.config(text=f"Your tries left: {self.tries}/6")
+        self.display_update()
+        if all(letter in self.guessed_letters for letter in self.word):
+            messagebox.showinfo('You won the game. The word is:',self.word)
+            self.enter.config(state='disabled')
+            self.enter_button.config(state='disabled')
+            return
+        if self.tries==0:
+            messagebox.showinfo('OOPS! You Lost this game. Better luck next time. The word was: ',self.word)
+            self.enter.config(state='disabled')
+            self.enter_button.config(state='disabled')
+            return
+    def reset_game(self):
+        self.word = random.choice(word_list)
+        self.guessed_letters=''
+        self.tries=6
+        self.enter.config(state='normal')
+        self.enter_button.config(state='active')
+        self.word_label.config(text='_ '*len(self.word))
+        self.tries_label.config(text=f'Your tries left: {self.tries}/6')
+        self.feedback_label.config(text='')
+    def on_back(self):
+        self.click= on_click()
+        destroy = self.roo.destroy
+        self.roo.after(300,destroy)
+root = tk.Tk()
+root.title('menu')
+root.geometry('500x500')
+rock_game_button = tk.Button(root,text='Rock Paper Scissors',width=15,command=lambda:[rockpaperscissor(),on_click()])
+rock_game_button.pack(pady=5)
+hangman_game_button = tk.Button(root,text='Hangman',width=15,command=lambda:[on_click(),Hangman()])
+hangman_game_button.pack(pady=5)
 def open_settings():
-    #change volume
     def change_volume(value):
-        global music_level
+        global music_length
         user_value = float(value)
         pygame.mixer.music.set_volume(user_value)
-        music_level=user_value
-    #for changing the mode to dark on clicking and then agin to lightblue on another click
-    def change_mode():
-        global dark_mode
-        if(dark_mode):
-            after_settings = settings_pop.config(bg='black')
-            after_feature = root.config(bg="black")
-            root.after(100,after_feature,after_settings)
-            dark_mode=False
-        else:
-            after_black = settings_pop.config(bg="lightblue")
-            change_light = root.config(bg='lightblue')
-            root.after(100,change_light,after_black)
-            dark_mode=True
-    #for back functionality
-    def back_game():
-        on_click()
-        destroy = settings_pop.destroy
-        settings_pop.after(300,destroy)
-    settings_pop = tk.Toplevel(root)
-    settings_pop.title("Settings")
-    settings_pop.geometry("300x300")
-    settings_pop.config(bg="lightblue")
-    vol_label = tk.Label(settings_pop,text="Change the Volume: ")
-    vol_label.pack(pady=10)
-    vol_change_slider = tk.Scale(settings_pop,from_=0,to=1,resolution=0.1,orient=tk.HORIZONTAL,command=change_volume)
-    vol_change_slider.set(music_level)
-    vol_change_slider.pack(padx=5)
-    play_music_button = tk.Button(settings_pop,text="Music",height=2,width=10,command=lambda:[on_click(),play_music()])
+        music_length=user_value
+    def toggle_color():
+        global enable_mode
+        enable_mode=not enable_mode
+        apply_to_any_window(root)
+        apply_to_any_window(settings_window)
+    def on_back():
+        click= on_click()
+        destroy = settings_window.destroy
+        settings_window.after(300,destroy)
+    def on_exit():
+        click = on_click()
+        destroy = root.destroy
+        root.after(300,destroy)
+    settings_window = tk.Toplevel(root)
+    settings_window.geometry('500x500')
+    volume_label = tk.Label(settings_window,text='Change Volume: ')
+    volume_label.pack(pady=5)
+    volume_change_slider = tk.Scale(settings_window,from_=0,to=1,resolution=0.1,orient=tk.HORIZONTAL,command=lambda value:change_volume(value))
+    volume_change_slider.set(music_length)
+    volume_change_slider.pack(pady=5)
+    change_mode_button = tk.Button(settings_window,text='Toggle',width=15,command=lambda:[on_click(),toggle_color()])
+    change_mode_button.pack(pady=5)
+    play_music_button = tk.Button(settings_window,text='Music',width=15,command=lambda:[on_click(),play_music()])
     play_music_button.pack(pady=5)
-    change_theme_button = tk.Button(settings_pop,text="Mode",height=2,width=10,command=lambda:[on_click(),change_mode()])
-    change_theme_button.pack(pady=5)
-    back_button = tk.Button(settings_pop,text='Back',height=2,width=10,command=back_game)
+    back_button = tk.Button(settings_window,text='Back',width=15,command=on_back)
     back_button.pack(pady=5)
-    log_out_button = tk.Button(settings_pop,text='Log Out',height=2,width=10,command=on_exit)
-    log_out_button.pack(pady=5)
-#buttons for above
-button_frame = tk.Frame(root,bg="#e6f7ff")
-button_frame.pack(pady=20)
-rock_button = tk.Button(button_frame,text='rock',height=2,width=10, command=lambda:[on_click(),play_game('rock')])
-rock_button.grid(row=0,column=0,padx=5)
-paper_button=tk.Button(button_frame,text='paper',height=2,width=10,command=lambda:[on_click(),play_game('paper')])
-paper_button.grid(row=0,column=1,padx=5)
-scissor_button = tk.Button(button_frame,text='scissors',height=2,width=10,command=lambda:[on_click(),play_game('scissors')])
-scissor_button.grid(row=0,column=2,padx=5)
-reset_button = tk.Button(button_frame,text="Reset",height=2,width=10,command=lambda:[on_click(),reset_game()])
-reset_button.grid(row=1,column=0,pady=5)
-settings_menu_button = tk.Button(button_frame,text="Settings",height=2,width=10,command=lambda:[on_click(),open_settings()])
-settings_menu_button.grid(row=1,column=1,pady=5)
+    exit_button = tk.Button(settings_window,text='Exit',width=15,command=on_exit)
+    exit_button.pack(pady=5)
+    apply_to_any_window(settings_window)
+settings_button = tk.Button(root,text='Settings',width=15,command=lambda:[on_click(),open_settings()])
+settings_button.pack(pady=5)
+apply_to_any_window(root)
 root.mainloop()
